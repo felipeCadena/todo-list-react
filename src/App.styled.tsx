2293f700259cafddfc91ee15dae8ec3c5e350438
felipeCadena <felipecaden@gmail.com>
2023-08-28 19:33:54 -0300

    estilizando

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -4,12 +4,20 @@ export const ContainerBody = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: space-around;
     gap: 1rem;
-    height: 40rem;
-    width: 40rem;
+    margin: 4rem auto;
+    height: 80vh;
+    width: 60vw;
+    background-color: ${props => props.theme.colors.borderColor};
+    border-radius: 3rem;
 
-    & input {
+    & h1 {
+      font-size: 2rem;
+    }
+
+    & .inputTask {
+      width: 25rem;
       border: none;
       border-radius: 1rem;
       padding: 1rem;
@@ -20,5 +28,15 @@ export const ContainerBody = styled.div`
       border: none;
       border-radius: 1rem;
       padding: 1rem;
+    }
+
+    & .task_done {
+      display: flex;
+      justify-content: space-evenly;
+      width: 60vw;
+
+      & p {
+        font-size: 0.8rem;
+      }
     }
 `;
--- src/components/AllTask/AllTask.tsx
+++ src/components/AllTask/AllTask.tsx
@@ -1,7 +1,9 @@
 import { Task } from '../../types'
-import {VscError} from 'react-icons/vsc'
 import { MdDoneOutline } from 'react-icons/md';
 import { ContainerMain, TaskLine} from './AllTask.styled'
+import { FaTrashAlt } from 'react-icons/fa'
+import {BiTimer} from 'react-icons/bi'
+// import React, { useState } from 'react';
 
 type PropsAllTask = {
   allTask: Task[],
@@ -10,24 +12,51 @@ type PropsAllTask = {
 }
 
 function AllTask({allTask, handleCheck, handleDelete}: PropsAllTask) {
+  // const [edit, setEdit] = useState(true);
+  // const [valueInput, setValueInput] = useState('')
+
+  // function handleChange({target}: React.ChangeEvent<HTMLInputElement>) {
+  //   setValueInput(target.value)
+  //   console.log(valueInput);
+    
+  // }
+
+  // function handleSalve() {
+  //   setEdit(!edit)
+  //   setValueInput(valueInput)
+  // }
+
   return (
     <ContainerMain>
     {allTask.map(({id, task, completed}) => (
-    <label key={id}>
+    <label className='label' key={id}>
           <input 
           type="checkbox"
           id={id}
+          checked={completed}
+          className='checkbox'
           onChange={() => handleCheck(id)}
           />
-          <TaskLine completed={completed}>{task}</TaskLine>
-          {!completed ? (
-          <VscError 
+          {completed ? <MdDoneOutline className='iconDone' size={20}/> : <BiTimer className='icon' size={20}/>}
+            <TaskLine completed={completed}>{task}</TaskLine>
+          {/* {edit ? (
+            <>
+              <button onClick={() => setEdit(!edit)}>Editar</button>
+            </>
+          ) : (
+            <>
+              <label>
+                <input type="text" onChange={handleChange}/>
+              </label>
+              <button onClick={handleSalve}>Salvar</button>
+            </>
+          )} */}
+          <FaTrashAlt 
+          className='iconTrash'
           onClick={() => handleDelete(id)} 
-          color='red' 
           title='Remover'
           size={15}
-          /> ) : 
-          <MdDoneOutline color='green' size={15} />}
+          />
         </label>
     ))}
   </ContainerMain>
